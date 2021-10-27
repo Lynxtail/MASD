d7eb56341f092770061c3de685123cb0630b7b05
Lynxtail <l.gorbachev@mail.ru>
2021-10-27 10:20:01 +0400

    task 5 done

--- masd5.py
+++ masd5.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.core.fromnumeric import shape
 import pingouin as pg
 from itertools import product
 
@@ -11,15 +12,15 @@ x = np.array(list(product(x, x)))
 
 ones = np.ones(n).reshape(-1, 1)
 x = np.hstack((ones, x))
-print(x)
+print(f'X =\n{x}')
 
 xtx = x.T.dot(x)
-print(xtx)
+print(f"X'X =\n{xtx}")
 xty = x.T.dot(y)
-print(xty)
+print(f"X'Y = {xty}")
 
 theta = np.linalg.inv(xtx).dot(xty)
-print(theta)
+print(f'Theta = {theta}')
 
 y_ = np.mean(y)
 
@@ -30,7 +31,7 @@ print(f'beta_0 (по формулам) = {beta0}\n\
 beta_1 (по формулам) = {beta1}\n\
 beta_2 (по формулам) = {beta2}')
 
-print(pg.linear_regression(x, y))
+print('\n',pg.linear_regression(x, y))
 
 print(f"beta_0 (с помощью pingouin) = {pg.linear_regression(x, y).loc[0].at['coef']}\n\
 beta_1 (с помощью pingouin) = {pg.linear_regression(x, y).loc[1].at['coef']}\n\
@@ -42,21 +43,21 @@ y = {beta0:.2f} + {beta1:.2f}x^(1) + {beta2:.2f}x^(2)')
 for i in range(n)]
 
 # косяки с транспонированием
-# thetat = theta.reshape(-1, 1)
-# xty = xty.reshape(3, 1)
-# thetat_xty = theta.dot(xty)
-# print(thetat_xty)
-# yty = y.reshape(-1, 1).dot(y)
-# print(yty)
+theta = np.array(theta)
+thetat = theta.T
+thetat_xty = theta.dot(xty)
+print(f"Theta'*X'Y = {thetat_xty:.2f}")
+yty = y.T.dot(y)
+print(f"Y'Y = {yty:.2f}")
 
-# R_2 = (thetat_xty - n * y_**2) / (yty - n * y_**2)
-# print(f'R^2 = {R_2}')
-# F = (R_2 * (n - q - 1)) / ((1 - R_2) * q)
+R_2 = (thetat_xty - n * y_**2) / (yty - n * y_**2)
+print(f'R^2 = {R_2}')
+F = (R_2 * (n - q - 1)) / ((1 - R_2) * q)
 
-# # v(2, 16 - 2 - 1) ~ 19.42
-# v_alpha = 19.42
-# if F > v_alpha:
-#     print('Уравнение регрессии значимо')
+# v(2, 16 - 2 - 1) ~ 19.42
+v_alpha = 19.42
+if F > v_alpha:
+    print('Уравнение регрессии значимо (с помощью формул)')
 
 alpha = 0.05
 if pg.linear_regression(x, y).loc[0].at['pval'] < alpha:
